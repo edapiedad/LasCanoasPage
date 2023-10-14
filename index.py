6ffebfec124081f72da29e9f73ae5f53b1055621
edapiedad <eliuddapiedad@gmail.com>
2023-10-13 20:28:55 -0400

    first update

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, abort, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -15,4 +15,4 @@ def services():
     return render_template('services.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(port=8000)
